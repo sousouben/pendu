7cf4d818f28d58bf6c1b3da66feb806602981d15
SOUSOUBENH <75803460+sousouben@users.noreply.github.com>
2024-02-23 14:43:05 +0100

    accélération du temps

--- script.js
+++ script.js
@@ -1,57 +1,58 @@
-//récuperer les classes dans le html
-let container = document.querySelector(".container");
-let btn = document.querySelector(".start_btn");
-let scoreContainer = document.querySelector(".score");
-let timeContainer = document.querySelector(".time");
+// Sélection des éléments HTML
+const container = document.querySelector(".container");
+const btn = document.querySelector(".start_btn");
+const scoreContainer = document.querySelector(".score");
+const timeContainer = document.querySelector(".time");
 
-//musique qu fait peur
+// Musique effrayante
 document.addEventListener("DOMContentLoaded", function () {
-  var backgroundMusic = document.getElementById("backgroundMusic");
+  const backgroundMusic = document.getElementById("backgroundMusic");
 
   // Lecture automatique après une petite interaction utilisateur
-  document.addEventListener("click", function () {
+  document.addEventListener("click", function playBackgroundMusic() {
     backgroundMusic.play();
     // Retirez l'écouteur d'événement après la première interaction
-    document.removeEventListener("click", arguments.callee);
+    document.removeEventListener("click", playBackgroundMusic);
   });
 
-  updateScoreDisplay();
+  updateScoreDisplay(); // Met à jour l'affichage du score initial
 });
 
+// Gestion du clic sur le bouton de démarrage
 btn.onclick = function () {
   let score = 0;
   let time = 20;
-  container.innerHTML = "";
+  container.innerHTML = ""; // Réinitialise le contenu du conteneur
 
-  let interval = setInterval(function showTarget() {
-    //creation de la cible
-    let target = document.createElement("img");
+  const interval = setInterval(function showTarget() {
+    // Création de la cible
+    const target = document.createElement("img");
     target.id = "target";
     target.src = "fantome.png";
     container.appendChild(target);
     target.style.top = Math.random() * (500 - target.offsetHeight) + "px";
     target.style.left = Math.random() * (600 - target.offsetWidth) + "px";
 
-    //faire disparaitre la cible
+    // Faire disparaître la cible après un certain délai
     setTimeout(function () {
       target.remove();
     }, 2000);
 
-    //quand on clique sur le target
+    // Gestion du clic sur la cible
     target.onclick = function () {
       score += 1;
       target.style.display = "none";
     };
     time -= 1;
 
-    //afficher les infos
+    // Affichage du score et du temps restant
     scoreContainer.innerHTML = `Score : ${score}`;
     timeContainer.innerHTML = `Temps : ${time}`;
 
-    //fin du je quand le temps est ecoule
-    if (time == 0) {
+    // Fin du jeu lorsque le temps est écoulé
+    if (time === 0) {
       clearInterval(interval);
-      container.innerHTML = "bien joué!";
+      container.innerHTML = "Bien joué!";
     }
-  }, 1000);
+  }, 500); // Délai réduit à 500 millisecondes
 };
